dd055e9b4447f5c855313349ecabf741f582f0ed
Anatoly Frolov <44891639+anafro@users.noreply.github.com>
2023-02-15 23:44:51 +1200

    Added new controllers

---
+++ app/Http/Controllers/AnimationObjectController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AnimationObject;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AnimationObjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AnimationObject $animationObject): Response
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AnimationObject $animationObject): Response
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AnimationObject $animationObject): RedirectResponse
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AnimationObject $animationObject): RedirectResponse
+    {
+        //
+    }
+}
